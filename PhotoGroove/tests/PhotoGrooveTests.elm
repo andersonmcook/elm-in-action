ec9c3985cb14b9f71abf34e7eed4eac61f35d706
Anderson Cook <andersonmcook@gmail.com>
2020-03-22 18:33:11 -0500

    some tests

--- PhotoGroove/tests/PhotoGrooveTests.elm
+++ PhotoGroove/tests/PhotoGrooveTests.elm
@@ -2,16 +2,54 @@ module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Json.Decode exposing (decodeString)
-import PhotoGroove
+import Html.Attributes as Attrs exposing (src)
+import Json.Decode as Decode exposing (decodeValue)
+import Json.Encode as Encode
+import PhotoGroove exposing (..)
 import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (attribute, tag, text)
 
 
 decoderTest : Test
 decoderTest =
-    test "title defaults to (untitled)" <|
-        \_ ->
-            """{"url": "abc.com", "size": 5}"""
-                |> decodeString PhotoGroove.photoDecoder
+    fuzz2 string int "title defaults to (untitled)" <|
+        \url size ->
+            [ ( "url", Encode.string url )
+            , ( "size", Encode.int size )
+            ]
+                |> Encode.object
+                |> decodeValue photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitled)")
+
+
+sliders : Test
+sliders =
+    describe "Slider sets the desired field in the model"
+        [ testSlider "SlidHue" SlidHue .hue
+        , testSlider "SlidNoise" SlidNoise .noise
+        , testSlider "SlidRipple" SlidRipple .ripple
+        ]
+
+
+testSlider : String -> (Int -> Msg) -> (Model -> Int) -> Test
+testSlider description toMsg amountFromModel =
+    fuzz int description <|
+        \amount ->
+            initialModel
+                |> update (toMsg amount)
+                |> Tuple.first
+                |> amountFromModel
+                |> Expect.equal amount
+
+
+noPhotosNoThumbnails : Test
+noPhotosNoThumbnails =
+    test "No thumbnails render when there are no photos to render." <|
+        \_ ->
+            initialModel
+                |> view
+                |> Query.fromHtml
+                |> Query.findAll [ tag "img" ]
+                |> Query.count (Expect.equal 0)
